fb1574d4f003cd18fc9076f273968554ea591218
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2018-06-18 16:21:15 +0200

    Two Enter On Catalog Activity

--- app/src/main/java/com/example/mahmoudhamdyae/mhnote/CatalogActivity.java
+++ app/src/main/java/com/example/mahmoudhamdyae/mhnote/CatalogActivity.java
@@ -76,6 +76,7 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         getLoaderManager().initLoader(NOTE_Loader, null, this);
     }
 
+
     /**
      * Helper method to delete all notes in the database.
      */
